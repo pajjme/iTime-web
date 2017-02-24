5d60ef1f5b8a3956adf4d14f7c49999cf6a1f634
Andreas Brommund <andreas@brommund.se>
2017-02-24 16:23:46 +0100

    Move js from html to js. Add window.onload

--- www/js/chart.js
+++ www/js/chart.js
@@ -1,4 +1,13 @@
-function displayPieChart(data,config,canvasId) {
+function displayPieChart(data,canvasId) {
+	
+	var config =  { 		               
+		animationSteps: 100,
+		animationEasing: 'easeInOutQuart',
+		labelFontSize: 20,
+		animateScale: true,
+		segmentStrokeWidth: 20,
+	};
+
 	var ctx = document.getElementById(canvasId).getContext("2d");
 	new Chart(ctx).Pie(data,config);
 }	
