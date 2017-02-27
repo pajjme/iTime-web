85d02ddf82ce9c95e72776bcbc69e72dba6c538e
Jonas Hongisto <hongisto@kth.se>
2017-02-27 18:23:54 +0100

    Makes buttons work and gets the json object for the server side authentication code.

--- www/js/authenticate.js
+++ www/js/authenticate.js
@@ -1,25 +1,22 @@
-var googleUser = {};
-  var startApp = function() {
+var startApp = function() {
+    //document.getElementById("gSignOutWrapper").style.visibility="hidden"
     gapi.load('auth2', function(){
-      // Retrieve the singleton for the GoogleAuth library and set up the client.
-      auth2 = gapi.auth2.init({
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
         client_id: '856845744679-fr7uheupsjm65udbao75b6no8vjl8cm0.apps.googleusercontent.com',
         cookiepolicy: 'single_host_origin',
-        // Request scopes in addition to 'profile' and 'email'
-        //scope: 'additional_scope'
-      });
-      attachSignin(document.getElementById('customBtn'));
-      auth2.attachClickHandler(document.getElementById('customBtn2'), {}, function() {auth2.disconnect();}, function() {window.alert(0)}); 
+        scope: 'https://www.googleapis.com/auth/calendar'
     });
-  };
+    //attachClickHandler(document.getElementById('customBtn1'), {}, function(googleUser) {googleUser.signIn();}, function() {window.alert(0)}); 
+    //attachSignin(document.getElementById('customBtn'));
+    auth2.attachClickHandler(
+        document.getElementById('customBtn'), //what document element is the click attached to
+        {}, //OPTIONS
+        function() {auth2.grantOfflineAccess().then(function(resp) {document.getElementById('gSignInWrapper').style.visibility="hidden";document.getElementById("gSignOutWrapper").style.visibility="visible"/*ADD function that sends resp repackaged with the email as a json*/})}, 
+        function(error) {window.alert(0)}
+    );
+    document.getElementById('customBtn2').addEventListener("click", function() {auth2.disconnect(); document.getElementById('gSignInWrapper').style.visibility="visible";document.getElementById("gSignOutWrapper").style.visibility="hidden"});
+    });
+};
 
-  function attachSignin(element) {
-    console.log(element.id);
-    auth2.attachClickHandler(element, {},
-        function(googleUser) {
-          document.getElementById('name').innerText = "Signed in: " +
-              googleUser.getBasicProfile().getName();
-        }, function(error) {
-          alert(JSON.stringify(error, undefined, 2));
-        });
-  }
+
