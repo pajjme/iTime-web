ef74dc3bc90f02c00d3f32a1c0754a4b4a4bf794
Jonas Hongisto <hongisto@kth.se>
2017-03-03 16:37:09 +0100

    fixed naming as per dskeppstedt suggestion

--- www/js/authenticate.js
+++ www/js/authenticate.js
@@ -1,25 +1,25 @@
 var startApp = function() {
 	gapi.load('auth2', function(){
-	// Retrieve the singleton for the GoogleAuth library and set up the client.
-	auth2 = gapi.auth2.init({
-		client_id: '856845744679-fr7uheupsjm65udbao75b6no8vjl8cm0.apps.googleusercontent.com',
-		cookiepolicy: 'single_host_origin',
-		scope: 'https://www.googleapis.com/auth/calendar'
-	});
-
-	var r = new htmlElementStyleManager(document.getElementById('gSignInWrapper'),document.getElementById("gSignOutWrapper"));
-	auth2.attachClickHandler(
-		document.getElementById('customBtn'), //what document element is the click attached to
-		{}, //OPTIONS
-		function() {auth2.grantOfflineAccess().then(function(resp) {
-			console.log(resp.code);
-			r.swapVisibility(0,1,"hidden","visible");
-			var s = new httpCommunicator();
-			var dataToBeSent = {auth_code: resp.code}
-			s.sendToServer("http://127.0.0.1:5000/login",dataToBeSent);
-		})}, 
-		function(error) {window.alert(0)}
-	);
-	document.getElementById('customBtn2').addEventListener("click", function() {auth2.disconnect(); r.swapVisibility(0,1,"visible","hidden")});
+		var startAppHttpCommunicator = new httpCommunicator();
+		var startAppHtmlElementManager = new htmlElementStyleManager(document.getElementById('gSignInWrapper'),document.getElementById("gSignOutWrapper"));
+		// Retrieve the singleton for the GoogleAuth library and set up the client.
+		auth2 = gapi.auth2.init({
+			client_id: '856845744679-fr7uheupsjm65udbao75b6no8vjl8cm0.apps.googleusercontent.com',
+			cookiepolicy: 'single_host_origin',
+			scope: 'https://www.googleapis.com/auth/calendar'
+		});
+	
+		auth2.attachClickHandler(
+			document.getElementById('customBtn'), //what document element is the click attached to
+			{}, //OPTIONS
+			function() {auth2.grantOfflineAccess().then(function(resp) {
+				console.log(resp.code);
+				startAppHtmlElementManager.swapVisibility(0,1,"hidden","visible");
+				var dataToBeSent = {auth_code: resp.code}
+				startAppHttpCommunicator.sendToServer("http://127.0.0.1:5000/login",dataToBeSent);
+			})}, 
+			function(error) {window.alert(0)}
+		);
+		document.getElementById('customBtn2').addEventListener("click", function() {auth2.disconnect(); startAppHtmlElementManager.swapVisibility(0,1,"visible","hidden")});
 	});
 };
