954f16a01fe959283c7cf663eacae9d07caf6bf5
Jonas Hongisto <hongisto@kth.se>
2017-03-04 21:31:47 +0100

    vet inte riktigt vad jag håller på med men bootstrap och flyttade runt lite saker. David kommer inte gilla detta commit meddelande

---
+++ www/js/defaultElems.js
@@ -0,0 +1,21 @@
+var pieData = [
+		{
+			value : 30,
+			color : "#F38630",
+			label : 'Sleep',
+			labelColor : 'white',
+		},
+		{
+			value : 30,
+			color : "#F34353",
+			label : 'Sleep',
+			labelColor : 'white',
+		},
+		{
+			value : 30,
+			color : "#F37364",
+			label : 'SleepHej',
+			labelColor : 'blue',
+		}
+
+];
