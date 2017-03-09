b790d25cfb09b994a6b5426df9437a4a9e2dd0cc
Andreas Brommund <andreas@brommund.se>
2017-03-09 19:03:25 +0100

    clean format

--- dev/js/load_index.js
+++ dev/js/load_index.js
@@ -4,28 +4,27 @@ window.onload = function() {
 	displayChart();
 };
 
-function displayChart(){
+function displayChart() {
 	var data = {
-    labels: [
-        "Red",
-        "Blue",
-        "Yellow"
-    ],
-    datasets: [
-        {
-            data: [300, 50, 100],
-            backgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
+		labels: [
+			"Red",
+			"Blue",
+			"Yellow"
+		],
+		datasets: [{
+			data: [300, 50, 100],
+			backgroundColor: [
+				"#FF6384",
+				"#36A2EB",
+				"#FFCE56"
 			],
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-            ]
-    	}]
-	};	
+			hoverBackgroundColor: [
+				"#FF6384",
+				"#36A2EB",
+				"#FFCE56"
+			]
+		}]
+	};
 
-	displayPieChart(data,"canvas");
+	displayPieChart(data, "canvas");
 }
